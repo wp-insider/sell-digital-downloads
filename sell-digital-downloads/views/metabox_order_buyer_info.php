3e6c0ddeb3100c748985453eaf59746ec4e62339
erommel <eminemwalsh@gmail.com>
2017-04-06 19:10:57 +0300

    * Fixed typos in readme.txt. * Changed language textdomain from "isell" to "sell-digital-downloads". * Changed debug log filename from "wp_isell_debug.log" to "wp-isell-debug.txt". * Added "Custom Thanks Page URL" option for each product.

--- sell-digital-downloads/views/metabox_order_buyer_info.php
+++ sell-digital-downloads/views/metabox_order_buyer_info.php
@@ -3,49 +3,49 @@ if (!current_user_can('edit_posts')) wp_die( __('You do not have sufficient perm
 ?>
 <table class="form-table">
 	<tr valign="top">
-       <th scope="row"><strong><label for="first_name"><?php echo __('First Name:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="first_name"><?php echo __('First Name:','sell-digital-downloads'); ?></label><strong></th>
        <td>
        		<input type="text" value="<?php echo $buyer_info['first_name']; ?>" id="first_name" name="first_name"  />
         </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="last_name"><?php echo __('Last Name:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="last_name"><?php echo __('Last Name:','sell-digital-downloads'); ?></label><strong></th>
        <td>
        		<input type="text" value="<?php echo $buyer_info['last_name']; ?>" id="last_name" name="last_name"   />
         </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="email"><?php echo __('Email:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="email"><?php echo __('Email:','sell-digital-downloads'); ?></label><strong></th>
        <td>
        		<input type="text" value="<?php echo $buyer_info['email']; ?>" id="email" name="email"   />
         </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="phone"><?php echo __('Phone:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="phone"><?php echo __('Phone:','sell-digital-downloads'); ?></label><strong></th>
        <td>
           <input type="text" value="<?php echo $buyer_info['phone']; ?>" id="phone" name="phone"  />
         </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="country"><?php echo __('Country:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="country"><?php echo __('Country:','sell-digital-downloads'); ?></label><strong></th>
        <td>
           <input type="text" value="<?php echo $buyer_info['country']; ?>" id="country" name="country"  />
         </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="state"><?php echo __('State:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="state"><?php echo __('State:','sell-digital-downloads'); ?></label><strong></th>
        <td>
           <input type="text" value="<?php echo $buyer_info['state']; ?>" id="state" name="state"  />
         </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="city"><?php echo __('City:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="city"><?php echo __('City:','sell-digital-downloads'); ?></label><strong></th>
        <td>
           <input type="text" value="<?php echo $buyer_info['city']; ?>" id="city" name="city"  />
         </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="zip"><?php echo __('Zip code:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="zip"><?php echo __('Zip code:','sell-digital-downloads'); ?></label><strong></th>
        <td>
           <input type="text" value="<?php echo $buyer_info['zip']; ?>" id="zip" name="zip"  />
         </td>
--- sell-digital-downloads/views/metabox_product_file.php
+++ sell-digital-downloads/views/metabox_product_file.php
@@ -6,27 +6,27 @@ if (!current_user_can('edit_posts')) wp_die( __('You do not have sufficient perm
 	<input type="hidden" name="product_token" />
 
     <tr valign="top">
-        <th scope="row"><strong><label for="isell_simple_download_checkbox"><?php echo __('Enable Simple Download:','isell'); ?></label><strong></th>
+        <th scope="row"><strong><label for="isell_simple_download_checkbox"><?php echo __('Enable Simple Download:','sell-digital-downloads'); ?></label><strong></th>
         <td align="left">
         <input name="isell_simple_download_checkbox" type="checkbox"<?php if(get_post_meta($post_id,'isell_simple_download_checkbox',true)=="yes") echo ' checked="checked"'; ?> value="yes"/>
-        <p class="description"><?php echo __('If you are having issues with downloading files on your server then you should check this option. It will use a simpler download method that should work.','isell'); ?></p>
+        <p class="description"><?php echo __('If you are having issues with downloading files on your server then you should check this option. It will use a simpler download method that should work.','sell-digital-downloads'); ?></p>
     </td>
     </tr>
     <tr valign="top">
-       <th scope="row"><strong><label for="product_file_name"><?php echo __('File Name:','isell'); ?></label><strong></th>
+       <th scope="row"><strong><label for="product_file_name"><?php echo __('File Name:','sell-digital-downloads'); ?></label><strong></th>
        <td>
        		<input type="text" value="<?php echo get_post_meta($post_id,'product_file_name',true); ?>" id="isell_product_file_name" name="product_file_name" required class="regular-text" />
-       		<p class="description"><?php echo __('File will be downloaded to the customer\'s computer with this name. Example: My-ebook.pdf (This field is Required if "Simple Download Option" is not enabled)','isell'); ?></p>
+       		<p class="description"><?php echo __('File will be downloaded to the customer\'s computer with this name. Example: My-ebook.pdf (This field is Required if "Simple Download Option" is not enabled)','sell-digital-downloads'); ?></p>
         </td>
     </tr>
     <input type="hidden" value='<?php echo wp_create_nonce("isell_file_upload"); ?>' name="isell_file_upload_nonce" id="isell_file_upload_nonce" />
     <input type="hidden" value='<?php echo $post_id; ?>' name="post_id" id="isell_product_id" />
     <tr valign="top">
-        <th scope="row"><strong><label for="product_file_url"><?php echo __('File URL:','isell'); ?></label><strong></th>
+        <th scope="row"><strong><label for="product_file_url"><?php echo __('File URL:','sell-digital-downloads'); ?></label><strong></th>
         <td>
             <input type="text" value="<?php echo get_post_meta($post_id,'product_file_url',true); ?>" id="product_file_url" name="product_file_url" size="90" />
-            <input class="button" id="product_file_upload_button" type="button" value="<?php echo __('Upload File','isell'); ?>" />
-            <p class="description"><?php echo __('Enter the URL of your downloadable file or upload one','isell'); ?></p>
+            <input class="button" id="product_file_upload_button" type="button" value="<?php echo __('Upload File','sell-digital-downloads'); ?>" />
+            <p class="description"><?php echo __('Enter the URL of your downloadable file or upload one','sell-digital-downloads'); ?></p>
         </td>
     </tr>
 
